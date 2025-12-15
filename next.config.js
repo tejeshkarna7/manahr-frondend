b2d1d5a0d9db9f185a62714cf7e449a4359ebd8c
tejeshkarna7 <tejeshkarna7@gmail.com>
2025-12-15 13:58:44 +0530

    fix: bugs

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.amazonaws.com',
-      },
-    ],
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-}
+    images: {
+        domains: ["localhost"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**.amazonaws.com",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
-export { auth as middleware } from '@/auth';
+export { auth as middleware } from "@/auth";
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.svg).*)'],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.svg).*)",
+  ],
 };
